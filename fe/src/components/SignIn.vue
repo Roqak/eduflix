a6d7ab088b607a1c997b427f58b6af2f4f86c518
Roqak <roqak1@gmail.com>
2019-01-22 15:41:03 +0100

    Added Axios for login

--- fe/src/components/SignIn.vue
+++ fe/src/components/SignIn.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+              <h1>Welcome {{token}}</h1>
+
   <v-container>
     <v-layout row class="text-xs-center">
       <v-flex xs3 style="background-image: url('http://cdn.wallpapersafari.com/7/86/gqiGH7.jpg')">
@@ -9,12 +11,13 @@
         <v-container style="position: relative;top: 13%;" class="text-xs-center">
           <v-card flat>
             <v-card-title primary-title>
-              <h4>SignIn</h4>
+              <h4>{{name}}</h4>
             </v-card-title>
             <v-form>
             <v-text-field prepend-icon="person" name="Email" label="Email"></v-text-field>
             <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
             <v-card-actions>
+
               <v-btn primary large block @click="dodo">Login</v-btn>
             </v-card-actions>
             </v-form>
@@ -32,6 +35,7 @@
     data: () => ({
       valid: true,
       name: 'SignIn',
+      token: ''
       
     }),
 
@@ -45,28 +49,29 @@
       },
       dodo(){
           alert('done')
-        //   axios.post('http://localhost:8000/user/login', {
-        //     username: 'akinkunmi',
-        //     password: 'pass1'
-        // })
-        // .then(function (response) {
-        //     console.log(response);
-        // })
-        // .catch(function (error) {
-        //     console.log(error.response);
-        // });
+          axios.post('http://localhost:4000/user/login/', {
+            username: 'akinkunmi',
+            password: 'pass1'
+        })
+        .then(function (response) {
+            console.log(response.data.token);
+            this.token = response.data.token;
+        })
+        .catch(function (error) {
+            console.log(error.response);
+        });
         // navigator.getBattery().then(function(result) {
         //   console.log(result)
         // });
-        fetch('http://localhost:4000/user/login/', {
-	        method: 'post'
-            }).then(function(response) {
-              console.log(response)
-        }).catch(function(err) {
-          // Error :(
-            alert("Error")
-            console.log(err)
-        });
+        // fetch('http://localhost:4000/user/login/', {
+	      //   method: 'post'
+        //     }).then(function(response) {
+        //       console.log(response)
+        // }).catch(function(err) {
+        //   // Error :(
+        //     alert("Error")
+        //     console.log(err)
+        // });
               }
     }
   }
